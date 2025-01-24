d03205cc5f8a38004d3fdc727e5632d5a6365557
JJJYeong <bss05007@naver.com>
2025-01-24 14:52:39 +0900

    scss 분리

--- front/src/Layout.jsx
+++ front/src/Layout.jsx
@@ -1,31 +1,16 @@
 import { Outlet } from 'react-router-dom';
+import '@styles/Layout.scss';
 
 const Layout = () => {
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-            <header style={{
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                right: 0,
-                height: 100,
-                background: 'lightyellow',
-                zIndex: 1000, // 맨 앞으로
-            }}>
+        <div className="layout">
+            <header className="header">
                 <div>Header</div>
             </header>
-            <main style={{
-                flexGrow: 1,
-                paddingTop: 100, // header 높이
-                overflowY: 'auto',
-            }}>
+            <main className="main">
                 <Outlet />
             </main>
-            <footer style={{
-                height: 100,
-                background: 'beige',
-                marginTop: 'auto',
-            }}>
+            <footer className="footer">
                 <div>Footer</div>
             </footer>
         </div>
